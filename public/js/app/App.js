e99750e6cd82c4b7d680aed798db5842b1488b46
Richard Nunes <richardnunes.gm@gmail.com>
2017-12-21 18:01:07 +0000

    Cleaning up.

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div style={{textAlign: 'center'}}>
-                <h1>This is my react app</h1>
-            </div>);
-    }
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>This is my react app</h1>
+      </div>);
+  }
 }
